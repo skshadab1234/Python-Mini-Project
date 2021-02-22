2c68ac7289d9ec29ea9672447a67d8c7bd15b4e7
skshadab1234 <59305073+skshadab1234@users.noreply.github.com>
2021-02-22 17:23:18 +0530

    Mini Projects

---
+++ python-auto-draw/main.py
@@ -0,0 +1,29 @@
+# open the paint web app and then select pencil and run your python file after 10 second it will automaticall draw rectangle
+import pyautogui
+import time
+
+# time to change tabs from editor to paint;
+time.sleep(10)
+
+# it will remain clicked till program ends;
+pyautogui.click()
+
+# can be varied according to convininence
+distance = 250
+
+while distance > 0:
+    # right
+    pyautogui.dragRel(distance, 0, duration = 0.1)
+    
+    distance -= 5
+
+    # down      
+    pyautogui.dragRel(0, distance, duration = 0.1)  
+
+    # left
+    pyautogui.dragRel(-distance, 0, duration = 0.1)
+
+    distance -= 5
+
+    #up
+    pyautogui.dragRel(0, -distance, duration = 0.1)
